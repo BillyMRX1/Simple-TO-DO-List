563cb8e4721d3ba4f7d63f8dd1aae93f50fb5ac7
BillyMRX1 <brilianadeputra@gmail.com>
2021-01-03 01:51:56 +0700

    Added Task Fragment and navigation

--- app/src/main/java/com/mrx/simpleto_dolist/ui/MainActivity.kt
+++ app/src/main/java/com/mrx/simpleto_dolist/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mrx.simpleto_dolist
+package com.mrx.simpleto_dolist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mrx.simpleto_dolist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/mrx/simpleto_dolist/ui/tasks/TaskFragment.kt
@@ -0,0 +1,7 @@
+package com.mrx.simpleto_dolist.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.mrx.simpleto_dolist.R
+
+class TaskFragment : Fragment(R.layout.fragment_tasks) {
+}
